2e1af1669684942d412e7e0c2eee98f848aa28ab
Admin <k.i.r.s@bk.ru>
2022-11-24 22:19:40 +0300

    ДЗ 12 выполнено

---
+++ src/Author.java
@@ -0,0 +1,16 @@
+public class Author {
+    private String authorName;
+    private String authorLastName;
+    public Author (String name, String lastName) {
+            this.authorName = name;
+            this.authorLastName = lastName;
+        }
+        public String getAuthorName() {
+            return this.authorName;
+        }
+
+        public String getAuthorLastName() {
+            return authorLastName;
+        }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,22 @@ public class Main {
         sarah.setAge(31);
         System.out.println("sarah.getAge() = " + sarah.getAge());
         Person john = new Person("John", 13);
+        System.out.println();
+
+        // Задание 1
+
+        Author herbertSchildt = new Author("Герберт", "Шилдт");
+        Author PaulDaugherty = new Author("Пол", "Доэрти");
+        Book javaLight = new Book("Java Руководство для начинающих", herbertSchildt, 2019);
+        Book javaFull = new Book("Java Полное руководство", herbertSchildt, 2018);
+        Book humanMachine = new Book("Человек + машина", PaulDaugherty, 2019);
+        System.out.println(javaLight.getName()+" "+javaLight.getYear()+ " " +javaLight.getAuthor());
+        System.out.println(javaFull.getName()+" "+javaFull.getYear()+ " " +javaFull.getAuthor());
+        System.out.println(humanMachine.getName()+" "+humanMachine.getYear()+ " " +humanMachine.getAuthor());
+        System.out.println();
+        System.out.println("Вышло новое издание книги"); // Изменение параметра объекта
+        javaLight.setYear(2021);
+        System.out.println(javaLight.getName()+" "+javaLight.getYear()+ " " +javaLight.getAuthor());
 
     }
 }
